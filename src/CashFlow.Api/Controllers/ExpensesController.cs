e7a560d267e2ea02284184c6f6dc2dc4e435281c
joaod <joaodeus400@gmail.com>
2025-10-06 09:32:10 -0300

    feat: understanding try catch with validations that return cleaner responses

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -11,10 +11,23 @@ namespace CashFlow.Api.Controllers
         [HttpPost]
         public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
         {
-            var useCase = new RegisterExpenseUseCase();
+           try
+           {
+                var useCase = new RegisterExpenseUseCase();
 
-            var response = useCase.Execute(request);
-            return Created(string.Empty, response);       
+                var response = useCase.Execute(request);
+
+                return Created(string.Empty, response);
+
+            } 
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "unknown error");
+            }
         }
     }
 }
