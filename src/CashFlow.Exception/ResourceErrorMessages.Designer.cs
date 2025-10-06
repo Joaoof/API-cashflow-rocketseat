e3602480a5fd88122dde8346163a8b05f24cbd5e
joaod <joaodeus400@gmail.com>
2025-10-06 16:48:04 -0300

    feat: add payment type resource

--- src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
+++ src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
@@ -79,6 +79,15 @@ namespace CashFlow.Exception {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Payment is not valid.
+        /// </summary>
+        public static string PAYMENT_TYPE_INVALID {
+            get {
+                return ResourceManager.GetString("PAYMENT_TYPE_INVALID", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a The tile is required.
         /// </summary>
         public static string TITLE_REQUIRED {
