e02f57349434727d5ac5da79b9ea85f9a288dd77
joaod <joaodeus400@gmail.com>
2025-10-07 08:49:49 -0300

    feat: first successful test

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTeste.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Request;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidatorTeste
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterExpenseValidator();
+
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "description",
+            Title = "title",
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+        };
+
+        var result = validator.Validate(request);
+
+        Assert.True(result.IsValid);
+
+    }
+}
