599d1bc655ab0ded3b33ee9ab20f08e50d73b158
joaod <joaodeus400@gmail.com>
2025-10-08 09:16:45 -0300

    feat: creating a Database Context

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContexto.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContexto: DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Host=localhost;Port=5435;Database=postgres;Username=postgres;Password=Senha123@";
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+}
